174a80b2923b93e71f4b65ee19e189f083af82d1
Corey Clatterbuck <corey.clatterbuck@gmail.com>
2021-11-23 13:13:29 -0800

    cleaned & tidied data import & oc_use, created oc_tidy and oc_meta

--- oc.R
+++ oc.R
@@ -4,8 +4,23 @@ library(readxl)
 library(here)
 library(gganimate)
 library(cowplot)
+library(janitor)
 
-oc_raw <- read_excel(here("data", "OceanConservancy_CA.xlsx"))
+oc_raw <- read_excel(here("data", "OceanConservancy_CA.xlsx"), 
+                     col_types = c("numeric", "text", "text", "text", "numeric", 
+                                   "text", "date", "text", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric",
+                                   "numeric", "numeric", "numeric", "numeric", "numeric"))
+
 colnames(oc_raw)
 str(oc_raw)
 head(oc_raw)
@@ -17,6 +32,7 @@ oc_use <- oc_raw %>%
   mutate(`Cleanup Date` = as.Date(`Cleanup Date`)) %>%
   dplyr::filter(`Cleanup Date` %in% ccd_dates | 
                   (`Cleanup Date` >= as.Date('2020-09-01') & `Cleanup Date` <= as.Date('2020-09-30')))
+levels(as.factor(oc_use$`Cleanup Date`)) ## double check
 
 
 ## remove explicitly non-plastic items and items that cannot be identified
@@ -25,4 +41,35 @@ oc_use <- oc_use %>%
   select(-any_of(rm_nonp))
 
 
-## create separate metadata table and tidy table
+## continue cleaning, exploring & filtering dataset
+oc_use <- janitor::clean_names(oc_use)
+colnames(oc_use)
+str(oc_use)
+n_distinct(oc_use$cleanup_id) ## should equal nobs in oc_use. Cleanup ID can be used as an index column
+
+
+## create separate metadata table and tidy table, which removes rows with values = 0
+oc_meta <- oc_use %>%
+  dplyr::select(cleanup_id:number_of_bags)
+
+oc_tidy <- oc_use %>%
+  dplyr::select(cleanup_id, zone, cleanup_date, people, miles, cigarette_butts:gloves_masks_ppe) %>%
+  pivot_longer(cols = cigarette_butts:gloves_masks_ppe, names_to = "item", values_to = "value") %>%
+  drop_na(value)
+oc_tidy$year <- as.numeric(format(oc_tidy$cleanup_date, "%Y"))
+
+## double-check: did this remove rows where no specific items were reported as Sarah K warned on Nov 15?
+zero_rows <- oc_use %>%
+  filter_at(vars(15:52), any_vars(! is.na(.)))
+n_distinct(zero_rows$cleanup_id)
+n_distinct(oc_tidy$cleanup_id) ## if equal, then yes. 
+
+
+## get overall & yearly ranks of items
+
+## get overall & yearly ranks of items normalized by # of people per effort & compare to above
+
+## county-level analysis
+
+
+  
